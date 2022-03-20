e47f622ff3b9b7ae1b5973a16975271d736a2f87
NathansStuff <n.odonnell@hotmail.com>
2022-03-20 12:11:21 +1100

    blog content loads

--- pages/news/[slug].js
+++ pages/news/[slug].js
@@ -1,25 +1,43 @@
+import NewsContent from 'components/NewsContent';
+import NewsHeader from 'components/NewsHeader';
 import { getNewsBySlug, getAllNews } from 'lib/api';
+import { Row, Col } from 'react-bootstrap';
+import { urlFor } from 'lib/api';
 
 const NewsPage = ({ news }) => {
-  return <h1>Hello Detail Page - {news.slug}</h1>;
+  return (
+    <Row>
+     
+      <Col md={{ span: 10, offset: 1 }}>
+     
+        <NewsHeader
+          title={news.title}
+          subtitle={news.subtitle}
+          coverImage={urlFor(news.coverImage).height(600).url()}
+          author={news.author}
+          date={news.date}
+        />
+        <hr />
+        <p>{JSON.stringify(news)}</p>
+        <NewsContent content={news.content} />
+      </Col>
+    </Row>
+  );
 };
 
-
-export async function getStaticProps({params}) {
+export async function getStaticProps({ params }) {
   const news = await getNewsBySlug(params.slug);
   return {
-    props: {news}
-  }
+    props: { news },
+  };
 }
 
 export async function getStaticPaths() {
   const news = await getAllNews();
   return {
-    paths: news?.map(b => ({params: {slug: b.slug}})),
-    fallback: false
-  }
+    paths: news?.map(b => ({ params: { slug: b.slug } })),
+    fallback: false,
+  };
 }
 
-
 export default NewsPage;
-
